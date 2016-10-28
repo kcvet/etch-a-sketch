215a36f23c9b2f7d12e4d53c1617bf532b01322e
Kevin Cvetezar <kc3536@student.uni-lj.si>
2016-10-28 12:07:36 +0200

    drawing works, still needs some custo features

--- jqery.js
+++ jqery.js
@@ -2,8 +2,32 @@ $(document).ready(function(){
 	var squares= 16*16;
 
 		for(var i=0;i<squares;i++){
-			$('#inner').append('<div class="squares" id="sq'+i+'"></div>')
+			$('#inner').append('<div class="squares" id="sq'+i+'"></div>');//ustvari 16*16grid
 		}
 
+		$('.squares').hover(function(){
+			
+			$('.squares').mouseover(function(){
+				var ids = $(this).attr('id');
+				$('#'+ids).css('background-color','black');
+			});
+			
+		},function(){
+
+		});
+
+		/*$('squares').hover(function(){ //elementi čez katere hoveram
+			$('.squares').mouseover(function(){ //dobim id od div
+				ids = $(this).attr('id');
+				 alert(this.id);
+			});
+			$(ids).css('background-color','black');
+
+
+
+		},function(){
+
+		});*/
 
 });
+
